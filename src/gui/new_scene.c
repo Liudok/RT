bd090ecff7455c247431afecb1b199fa807f9886
Semen KAMOZA <skamoza@e2r6p10.unit.ua>
2018-05-11 22:49:25 +0300

    normed some stuff

--- src/gui/new_scene.c
+++ src/gui/new_scene.c
@@ -6,7 +6,7 @@
 /*   By: lberezyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/28 17:06:32 by lberezyn          #+#    #+#             */
-/*   Updated: 2018/04/25 12:44:19 by lberezyn         ###   ########.fr       */
+/*   Updated: 2018/05/11 22:48:12 by skamoza          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,64 +14,61 @@
 
 void				show_new_scene_panel(t_rt *s)
 {
-	SDL_Texture* bg;
-	SDL_Rect backgroundPos;
+	SDL_Texture		*bg;
+	SDL_Rect		background_pos;
 
 	bg = s->textures[1];
-	backgroundPos.y = 74;
-	backgroundPos.w = 190;
-	backgroundPos.h = 140;
-	backgroundPos.x = 0;
-	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &backgroundPos);
-	backgroundPos.y = 218;
-	backgroundPos.w = 190;
-	backgroundPos.h = 80;
-	backgroundPos.x = 0;
-	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &backgroundPos);
+	background_pos.y = 74;
+	background_pos.w = 190;
+	background_pos.h = 140;
+	background_pos.x = 0;
+	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &background_pos);
+	background_pos.y = 218;
+	background_pos.w = 190;
+	background_pos.h = 80;
+	background_pos.x = 0;
+	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &background_pos);
 }
 
 void				after_effect_panel(t_rt *s)
 {
-	SDL_Texture* bg;
-	SDL_Rect backgroundPos;
-	SDL_Rect		ram;
-	int				i;
+	const SDL_Texture		*bg = s->textures[1];
+	SDL_Rect				background_pos;
+	SDL_Rect				ram;
+	int						i;
 
-	bg = s->textures[1];
-	backgroundPos.y = 0;
-	backgroundPos.w = 230;
-	backgroundPos.h = 70;
-	backgroundPos.x = 192;
-	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &backgroundPos);
-	i = 16;
-	while (i < BUTTONS_AMOUNT)
-	{
+	background_pos.y = 0;
+	background_pos.w = 230;
+	background_pos.h = 70;
+	background_pos.x = 192;
+	SDL_RenderCopy(s->sdl.renderer, bg, NULL, &background_pos);
+	i = 15;
+	while (++i < BUTTONS_AMOUNT)
 		if (!s->buttons[i].pressed)
-			SDL_RenderCopy(s->sdl.renderer, s->buttons[i].txt, NULL, &s->buttons[i].rect);
+			SDL_RenderCopy(s->sdl.renderer,
+					s->buttons[i].txt, NULL, &s->buttons[i].rect);
 		else
 		{
 			ram.x = s->buttons[i].rect.x - 5;
 			ram.y = s->buttons[i].rect.y - 5;
 			ram.w = s->buttons[i].rect.w + 10;
 			ram.h = s->buttons[i].rect.h + 10;
-			SDL_RenderCopy(s->sdl.renderer, s->buttons[i].txt, NULL, &s->buttons[i].rect);
+			SDL_RenderCopy(s->sdl.renderer,
+					s->buttons[i].txt, NULL, &s->buttons[i].rect);
 			SDL_RenderCopy(s->sdl.renderer, s->textures[2], NULL, &ram);
 		}
-		i++;
-	}
 }
 
-
-void		create_new_scene(t_rt *s)
+void				create_new_scene(t_rt *s)
 {
-
-	show_new_scene_panel(s);
 	int				i;
 
+	show_new_scene_panel(s);
 	i = 4;
 	while (i < 16)
 	{
-		SDL_RenderCopy(s->sdl.renderer, s->buttons[i].txt, NULL, &s->buttons[i].rect);
+		SDL_RenderCopy(s->sdl.renderer, s->buttons[i].txt, NULL,
+				&s->buttons[i].rect);
 		i++;
 	}
 }
